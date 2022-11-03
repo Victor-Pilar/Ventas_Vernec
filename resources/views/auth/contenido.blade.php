2940107250c0575be7dca8c7536b79e8a001f45d
Victor Pilar Jimenez <victor.pilar.jimenez@gmail.com>
2022-11-02 21:34:54 -0600

    Change Login style

--- resources/views/auth/contenido.blade.php
+++ resources/views/auth/contenido.blade.php
@@ -5,13 +5,19 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Sistema Ventas Laravel Vue Js- compartiendocodigos">
-  <meta name="author" content="compartiendocodigos.net">
-  <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
+  <meta name="description" content="Sistema de ventas y factura - Vernec">
+  <meta name="author" content="Victor Pilar">
 
-  <title>Sistema Ventas - Compartiendo Códigos</title>
+  <title>Sistema Ventas - Vernec</title>
 
   <link href="css/plantilla.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="./styleLogin.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
 
@@ -22,6 +28,7 @@
 
   <!-- Bootstrap and necessary plugins -->
   <script src="js/plantilla.js"></script>
+  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
 </body>
 </html>
